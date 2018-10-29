76a604f9d1da689294b14a79c6cd07ae1734146e
Russell Porter <russell.porter@scout24.com>
2018-10-29 14:26:28 +0100

    Initial Commit

---
+++ ReproFBSnapshotTestCaseProblem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReproFBSnapshotTestCaseProblem
+//
+//  Created by Russell Porter on 10/29/18.
+//  Copyright © 2018 Russell Porter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
